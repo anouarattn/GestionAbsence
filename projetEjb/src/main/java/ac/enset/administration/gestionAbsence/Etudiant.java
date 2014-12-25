620708350bfd550204b740c54e839e97c9cff0a8
anouarattn <win7@192.168.80.128>
2014-12-25 14:54:51 +0100

    ajout entity etudiant

---
+++ projetEjb/src/main/java/ac/enset/administration/gestionAbsence/Etudiant.java
@@ -0,0 +1,13 @@
+package ac.enset.administration.gestionAbsence;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class Etudiant {
+	
+	@Id
+	private Long id;
+
+}
