077a15974165c19ab64c68c6246388599d3f7cec
anouarattn <bakri.anouar@outlook.com>
2015-01-10 14:26:14 +0000

    ajout colonne modifier dans la datatable departement

--- projetEjb/src/main/java/ac/enset/administration/gestionAbsence/entites/AnneeScolaire.java
+++ projetEjb/src/main/java/ac/enset/administration/gestionAbsence/entites/AnneeScolaire.java
@@ -4,14 +4,7 @@ package ac.enset.administration.gestionAbsence.entites;
 import java.io.Serializable;
 import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
 @Entity
