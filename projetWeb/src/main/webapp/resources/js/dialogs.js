f485d4b7ad98f969c2363beda30842acce65d2ba
anouarattn <bakri.anouar@outlook.com>
2015-01-26 16:28:57 +0000

    refactoring xhtml files : ajout de la fonctionnalité Resource Bundles/couplage moins fort

---
+++ projetWeb/src/main/webapp/resources/js/dialogs.js
@@ -0,0 +1,10 @@
+function handleRequestAdd(xhr, status, args) {
+	PF('jsAdd').hide();
+}
+
+function handleRequestModify(xhr, status, args) {
+	PF('jsModify').hide();
+}
+function handleRequestshow(xhr, status, args) {
+    PF('jsShow').hide();      
+}
